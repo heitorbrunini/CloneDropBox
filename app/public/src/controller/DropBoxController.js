c3064ccbf4c8c042f0ffdfc05e3473bf353ab08a
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-02-03 13:34:06 -0300

    PromiseAll

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,7 +3,7 @@ class DropBoxController{
     constructor(){
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
-        this.reactSnakeBarRootEL = document.querySelector('#react-snackbar-root')
+        this.snakeBarEL = document.querySelector('#react-snackbar-root')
 
 
         this.initEvents();
@@ -12,6 +12,45 @@ class DropBoxController{
     initEvents(){  
         this.btnSendFileEl.addEventListener('click', (e) =>{
             this.inputFilesEl.click();
+        });
+
+        this.inputFilesEl.addEventListener('change', event =>{
+
+            this.uploadTask(event.target.files);
+            this.snakeBarEL.style.display='block';
+
         })
     }
+
+    uploadTask(files){
+
+        let promises = [];
+        [...files].forEach( file =>{
+            promises.push(new Promise((resolve, reject) =>{
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST','/upload');
+                
+                //function will be done in the send
+                ajax.onload = event =>{
+                    try {
+                        resolve (JSON.parse((ajax.responseText)));
+                    }catch(error){
+                        reject(error);
+                    }
+                }
+
+                ajax.onerror= e => {
+                    reject(e)
+                }
+
+                let formData = new FormData();
+                formData.append('input-file',file);
+                ajax.send(formData);
+                
+            }));
+        })
+
+        return Promise.all(promises);
+    }
+
 }
