f2998216b6537c8f2643caf77968a59906039d7b
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-02-04 18:59:31 -0300

    Progress Bar

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,7 +3,11 @@ class DropBoxController{
     constructor(){
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
-        this.snakeBarEL = document.querySelector('#react-snackbar-root')
+        this.snakeBarEL = document.querySelector('#react-snackbar-root');
+        
+        this.progressBarEl = this.snakeBarEL.querySelector('.mc-progress-bar-fg');
+        this.namefileEL = this.snakeBarEL.querySelector('.filename');
+        this.timeleftEL = this.snakeBarEL.querySelector('.timeleft');
 
 
         this.initEvents();
@@ -43,14 +47,64 @@ class DropBoxController{
                     reject(e)
                 }
 
+                ajax.upload.onprogress = event => {
+                    this.uploadProgress(event,file);
+                }
+
                 let formData = new FormData();
                 formData.append('input-file',file);
+
+                this.startUploadTime = Date.now();
+                
                 ajax.send(formData);
                 
             }));
         })
 
         return Promise.all(promises);
+    }
+
+    uploadProgress(event,file){
+
+        let limespent = Date.now() - this.startUploadTime;
+
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt( (loaded / total) * 100);
+
+        let timeleft = ((100 - porcent) * limespent) / porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`;
+        this.namefileEL.innerHTML = file.name;
+        this.timeleftEL.innerHTML = this.formatTimeToHuman(timeleft);
+
+        if (this.progressBarEl.style.width == '100%'){
+            this.snakeBarEL.style.display='none';
+            this.progressBarEl.style.width ='0%';
+        }        
+    }
+
+    formatTimeToHuman(duration){
+
+        let seconds = parseInt( (duration/1000) % 60);
+        let minutes =parseInt ((duration /(1000*60)) %60);
+        let hours = parseInt ((duration /(1000*60 *60)) %24);
+        
+        if(hours >0){
+
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos restantes`
+        };
+
+        if(minutes >0){
+
+            return `${minutes} minutos e ${seconds} segundos restantes`
+        };
+
+        if(seconds >0){
+            return `${seconds} segundos restantes`
+        };
+
+        return '0 segundos restantes';
     }
 
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+/* GET home page. */
+router.post('/upload', function(req, res, next) {
+  
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  form.parse(req , (err, fields, files)=>{
+    err? console.log(err) :
+    res.json({files});
+  })
+
+});
+
+module.exports = router; 
